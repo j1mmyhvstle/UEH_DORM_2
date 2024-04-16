6ae7f20ae0fe073224ad46976df5e321ef376ce7
VuaVungCao <125594828+j1mmyhvstle@users.noreply.github.com>
2024-04-17 01:25:12 +0700

    func 10 : Thống kê

---
+++ Statistics.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace ChuongTrinhQuanLyKyTuXa_Version3
+{
+    partial class Statistics
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtp = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txtnvnl = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtnvl = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtsvd = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtsvo = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txto = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtp
+            // 
+            this.txtp.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(95)))), ((int)(((byte)(105)))));
+            this.txtp.BorderRadius = 15;
+            this.txtp.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtp.DefaultText = "";
+            this.txtp.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtp.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtp.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtp.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtp.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtp.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtp.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtp.Location = new System.Drawing.Point(220, 118);
+            this.txtp.Margin = new System.Windows.Forms.Padding(2);
+            this.txtp.Name = "txtp";
+            this.txtp.PasswordChar = '\0';
+            this.txtp.PlaceholderText = "";
+            this.txtp.SelectedText = "";
+            this.txtp.Size = new System.Drawing.Size(269, 29);
+            this.txtp.TabIndex = 36;
+            // 
+            // txtnvnl
+            // 
+            this.txtnvnl.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(95)))), ((int)(((byte)(105)))));
+            this.txtnvnl.BorderRadius = 15;
+            this.txtnvnl.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtnvnl.DefaultText = "";
+            this.txtnvnl.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtnvnl.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtnvnl.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtnvnl.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtnvnl.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtnvnl.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtnvnl.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtnvnl.Location = new System.Drawing.Point(220, 318);
+            this.txtnvnl.Margin = new System.Windows.Forms.Padding(2);
+            this.txtnvnl.Name = "txtnvnl";
+            this.txtnvnl.PasswordChar = '\0';
+            this.txtnvnl.PlaceholderText = "";
+            this.txtnvnl.SelectedText = "";
+            this.txtnvnl.Size = new System.Drawing.Size(269, 29);
+            this.txtnvnl.TabIndex = 33;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(48, 328);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(163, 19);
+            this.label7.TabIndex = 32;
+            this.label7.Text = "Nhân viên đã nghỉ làm:";
+            // 
+            // txtnvl
+            // 
+            this.txtnvl.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(95)))), ((int)(((byte)(105)))));
+            this.txtnvl.BorderRadius = 15;
+            this.txtnvl.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtnvl.DefaultText = "";
+            this.txtnvl.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtnvl.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtnvl.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtnvl.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtnvl.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtnvl.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtnvl.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtnvl.Location = new System.Drawing.Point(215, 278);
+            this.txtnvl.Margin = new System.Windows.Forms.Padding(2);
+            this.txtnvl.Name = "txtnvl";
+            this.txtnvl.PasswordChar = '\0';
+            this.txtnvl.PlaceholderText = "";
+            this.txtnvl.SelectedText = "";
+            this.txtnvl.Size = new System.Drawing.Size(274, 29);
+            this.txtnvl.TabIndex = 31;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(64, 288);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(147, 19);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "Nhân viên đang làm:";
+            // 
+            // txtsvd
+            // 
+            this.txtsvd.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(95)))), ((int)(((byte)(105)))));
+            this.txtsvd.BorderRadius = 15;
+            this.txtsvd.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtsvd.DefaultText = "";
+            this.txtsvd.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtsvd.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtsvd.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtsvd.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtsvd.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtsvd.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtsvd.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtsvd.Location = new System.Drawing.Point(220, 238);
+            this.txtsvd.Margin = new System.Windows.Forms.Padding(2);
+            this.txtsvd.Name = "txtsvd";
+            this.txtsvd.PasswordChar = '\0';
+            this.txtsvd.PlaceholderText = "";
+            this.txtsvd.SelectedText = "";
+            this.txtsvd.Size = new System.Drawing.Size(269, 29);
+            this.txtsvd.TabIndex = 29;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(100, 248);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 19);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Sinh viên đã đi:";
+            // 
+            // txtsvo
+            // 
+            this.txtsvo.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(95)))), ((int)(((byte)(105)))));
+            this.txtsvo.BorderRadius = 15;
+            this.txtsvo.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtsvo.DefaultText = "";
+            this.txtsvo.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtsvo.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtsvo.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtsvo.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtsvo.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtsvo.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtsvo.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtsvo.Location = new System.Drawing.Point(220, 199);
+            this.txtsvo.Margin = new System.Windows.Forms.Padding(2);
+            this.txtsvo.Name = "txtsvo";
+            this.txtsvo.PasswordChar = '\0';
+            this.txtsvo.PlaceholderText = "";
+            this.txtsvo.SelectedText = "";
+            this.txtsvo.Size = new System.Drawing.Size(269, 29);
+            this.txtsvo.TabIndex = 27;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(87, 209);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 19);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Sinh viên đang ở:";
+            // 
+            // txto
+            // 
+            this.txto.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(95)))), ((int)(((byte)(105)))));
+            this.txto.BorderRadius = 15;
+            this.txto.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txto.DefaultText = "";
+            this.txto.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txto.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txto.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txto.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txto.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txto.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txto.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txto.Location = new System.Drawing.Point(215, 156);
+            this.txto.Margin = new System.Windows.Forms.Padding(2);
+            this.txto.Name = "txto";
+            this.txto.PasswordChar = '\0';
+            this.txto.PlaceholderText = "";
+            this.txto.SelectedText = "";
+            this.txto.Size = new System.Drawing.Size(274, 29);
+            this.txto.TabIndex = 25;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(117, 166);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 19);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Phòng đã ở :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(110, 128);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 19);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Phòng trống :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(235, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 30);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "THỐNG KÊ";
+            // 
+            // Statistics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 373);
+            this.Controls.Add(this.txtp);
+            this.Controls.Add(this.txtnvnl);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtnvl);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtsvd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtsvo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Statistics";
+            this.Text = "Statistics";
+            this.Load += new System.EventHandler(this.Statistics_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2TextBox txtp;
+        private Guna.UI2.WinForms.Guna2TextBox txtnvnl;
+        private System.Windows.Forms.Label label7;
+        private Guna.UI2.WinForms.Guna2TextBox txtnvl;
+        private System.Windows.Forms.Label label6;
+        private Guna.UI2.WinForms.Guna2TextBox txtsvd;
+        private System.Windows.Forms.Label label5;
+        private Guna.UI2.WinForms.Guna2TextBox txtsvo;
+        private System.Windows.Forms.Label label4;
+        private Guna.UI2.WinForms.Guna2TextBox txto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Statistics.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChuongTrinhQuanLyKyTuXa_Version3
+{
+    public partial class Statistics : Form
+    {
+        Dashboard dashboardForm = new Dashboard();
+        function fn = new function();
+        string query;
+
+        public Statistics()
+        {
+            InitializeComponent();
+        }
+
+        private void Statistics_Load(object sender, EventArgs e)
+        {
+            // Đảm bảo chuỗi truy vấn được viết đúng cú pháp SQL và sử dụng dấu nháy đơn (' ') cho giá trị của cột roomStatus
+            query = "SELECT COUNT(*) FROM rooms WHERE roomStatus = 'Available' AND Booked = 'No';";
+
+            // Gọi hàm getData từ đối tượng function để thực hiện truy vấn và nhận kết quả trả về vào một DataSet
+            DataSet ds = fn.getData(query);
+
+            // Sau khi nhận được kết quả, bạn có thể xử lý nó ở đây, ví dụ, hiển thị trong một TextBox:
+            if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+            {
+                // Giả sử kết quả trả về là một số nguyên, bạn có thể lấy nó từ hàng đầu tiên của bảng kết quả:
+                int availableRoomCount = Convert.ToInt32(ds.Tables[0].Rows[0][0]);
+
+                // Hiển thị số lượng phòng trống trong một TextBox có tên là txtp
+                txtp.Text = availableRoomCount.ToString();
+            }
+
+            // Thực hiện truy vấn tương tự để lấy số lượng phòng đã đặt
+            query = "SELECT COUNT(*) FROM rooms WHERE roomStatus = 'Available' AND Booked = 'Yes';";
+
+            ds = fn.getData(query);
+
+            if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+            {
+                int bookedRoomCount = Convert.ToInt32(ds.Tables[0].Rows[0][0]);
+
+                // Hiển thị số lượng phòng đã đặt trong một TextBox có tên là txtpBooked
+                txto.Text = bookedRoomCount.ToString();
+            }
+            query = "SELECT COUNT(*) FROM Student WHERE living = 'Yes';";
+
+            ds = fn.getData(query);
+
+            if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+            {
+                int studentlive = Convert.ToInt32(ds.Tables[0].Rows[0][0]);
+
+                txtsvo.Text = studentlive.ToString();
+            }
+            query = "SELECT COUNT(*) FROM Student WHERE living = 'No';";
+
+            ds = fn.getData(query);
+
+            if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+            {
+                int studentmove = Convert.ToInt32(ds.Tables[0].Rows[0][0]);
+
+                txtsvd.Text = studentmove.ToString();
+
+            }
+            query = "SELECT COUNT(*) FROM newEmployee WHERE working = 'No';";
+
+            ds = fn.getData(query);
+
+            if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+            {
+                int unemployee = Convert.ToInt32(ds.Tables[0].Rows[0][0]);
+
+                txtnvnl.Text = unemployee.ToString();
+
+            }
+            query = "SELECT COUNT(*) FROM newEmployee WHERE working = 'Yes';";
+
+            ds = fn.getData(query);
+
+            if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+            {
+                int employee = Convert.ToInt32(ds.Tables[0].Rows[0][0]);
+
+                txtnvl.Text = employee.ToString();
+
+            }
+        }
+
+    }
+}
